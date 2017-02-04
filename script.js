6b25daed7bf1058dd663ad59f5f4042ba4ea79df
Ricky Simmons <Ricky Simmons>
2017-02-04 16:28:20 -0700

    adding files and variable work - unfinished

---
+++ script.js
@@ -0,0 +1,15 @@
+// Code goes here
+
+var variables = {
+  back: '<a href="index.html">Select Operator</a>',
+  counters: 'Counters',
+  countered: 'Countered By',
+  team: 'Team With',
+  avoid: 'Avoid When',
+  smoke: 'Smoke',
+  fuzename: 'Fuze'
+};
+
+for(var v in variables) {
+  document.getElementById(v).innerHTML = variables[v];
+}
